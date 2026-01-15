b9c35242669937e5542bee83129c168bd96eb124
Ironboy <ironboy@users.noreply.github.com>
2026-01-15 11:50:59 +0100

    refactored to functions

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-// Read products from json file
-let products = await(await fetch('products.json')).json();
+function createMainElements() {
+  // Create an main element, append to body and return
+  let main = document.createElement('main');
+  document.querySelector('body').append(main);
+  return main;
+}
 
-// Create an main element
-let main = document.createElement('main');
+// In order to use await inside function we must tell 
+// the JavaScript engine that we'll do that by adding async
+// to the function declaration
+async function displayProducts(main) {
+  // Read products from json file
+  let products = await (await fetch('products.json')).json();
 
-// Add som content inside main
-main.innerHTML = `
+  // Add som content inside main
+  main.innerHTML = `
   <h1>Our products</h1>
   <p>Some of our amazing products:</p>
   ${products.map(({ name, description, price }) => /*html*/`
@@ -16,6 +24,7 @@ main.innerHTML = `
     </article>
   `).join('')}
 `;
+}
 
-// Append main to body
-document.querySelector('body').append(main);
+let main = createMainElements();
+displayProducts(main);
